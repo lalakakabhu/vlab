21f95a1afb396b8a8750fffc7222a855c110c6ef
lalakakabhu <105699827+lalakakabhu@users.noreply.github.com>
2022-05-17 12:34:24 +0530

    Update vlab.py

--- vlab.py
+++ vlab.py
@@ -1,20 +1,44 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
-import streamlit as st
+from bokeh.models import ColumnDataSource, CustomJS, Slider
+from bokeh.plotting import figure, show, ColumnDataSource
+from bokeh.layouts import column
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Voltage", 1, 50, 200)
-    num_turns = st.slider("Current", 1, 100, 9) 
-    Point = namedtuple('Point', 'x y') 
-    data = []
-    resistance = total_points / num_turns
-    for curr_point_num in range(total_points):
-        x = total_points
-        y = num_turns
-        data.append(Point(x, y))
-        
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-                    .encode(x='x:Q', y='y:Q'))
+df = pd.DataFrame({'x':[11,12,13,14,15],'y':[22,23,24,25,26],'threshold':[1,2,3,4,5]})
+data = df.to_dict()
+
+source = ColumnDataSource(data=df)
+
+plot = figure(plot_width=400, plot_height=400)
+plot.circle('x', 'y', source=source)
+
+slider = Slider(start=1, end=5, value=1, step=1, title="threshold")
+
+callback = CustomJS(
+
+    args=dict(source=source, slider=slider, data=data),
+
+    code="""
+            var index = [];
+            var x = [];
+            var y = [];
+            var thresh = [];
+            for (var i = 0; i < Object.keys(data.threshold).length; i++) {
+                if(slider.value <= data.threshold[i]) {
+                    index.push(i);
+                    x.push(data.x[i]);
+                    y.push(data.y[i]);
+                    thresh.push(data.threshold[i]);
+                }
+            }
+            source.data.index = index;
+            source.data.x = x;
+            source.data.y = y;
+            source.data.threshold = thresh;
+            source.change.emit();
+         """
+)
+
+slider.js_on_change('value', callback)
+
+show(column(slider,plot))
