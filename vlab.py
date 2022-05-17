f27da05de8a6e0165e09881587dac502bfdcf12b
lalakakabhu <105699827+lalakakabhu@users.noreply.github.com>
2022-05-17 12:06:47 +0530

    Update vlab.py

--- vlab.py
+++ vlab.py
@@ -6,8 +6,8 @@ import streamlit as st
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9) 
+    total_points = st.slider("Voltage", 1, 50, 200)
+    num_turns = st.slider("Current", 1, 100, 9) 
     Point = namedtuple('Point', 'x y') 
     data = []
     points_per_turn = total_points / num_turns
