2a03d129047c4e8dff08e84aca244bca4917cb54
lalakakabhu <105699827+lalakakabhu@users.noreply.github.com>
2022-05-17 11:54:22 +0530

    Update vlab.py

--- vlab.py
+++ vlab.py
@@ -1,3 +1,11 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
+
+with st.echo(code_location='below'):
 total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
